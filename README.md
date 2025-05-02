c0ea55df8596e486233d3a6d10ee2fdc2fc9c179
kubyllka <kkaterynabilyk@gmail.com>
2025-05-02 18:57:12 +0300

    Draft FastAPI with validation and segmentation/classification model

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Skin-cancer-detection
+# Skin-cancer-detection API MOLETICA
+
+1. **Python 3.x** (Recommended version: 3.11)
+
+2. Install all necessary dependencies by running:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+   This will install **FastAPI** and other libraries, including the ones needed for your project to run correctly.
---
+++ config.py
@@ -0,0 +1,12 @@
+CLASS_LABELS = {
+    0: "Actinic Keratoses and Intraepithelial Carcinoma / Bowen's Disease (AKIEC)",
+    1: "Basal Cell Carcinoma (BCC)",
+    2: "Benign Keratosis-like Lesions (BKL)",
+    3: "Dermatofibroma (DF)",
+    4: "Melanoma (MEL)",
+    5: "Melanocytic Nevi (NV)",
+    6: "Vascular Lesions (VASC)",
+}
+
+DETECTION_TYPE = "skin-cancer"
+RESULTS_FOLDER = "results"
